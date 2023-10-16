72d7efa93e57aac4c4f4ef41e19c5085ec879b67
Sk ANjarul Hossain <skanjarulhossain347@gmail.com>
2023-10-16 14:04:09 +0530

    Metirial UI added

--- movies/src/App.js
+++ movies/src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        Anjarul
-      </h1>
+      <Header/>
+        
     </div>
   );
 }
